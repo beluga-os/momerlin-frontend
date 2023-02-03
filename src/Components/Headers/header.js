c7a96216a010cade5ff346d8fc60d50574f3c53f
mithranjith <mithran.ranjith@gmail.com>
2023-02-03 15:17:34 +0530

    Code cleanup

--- src/Components/Headers/header.js
+++ src/Components/Headers/header.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
-// import MenuIcon from '@material-ui/icons/Menu';
-import Logo from "../../assets/logo.png"
-// import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
-import MenuItem from '@material-ui/core/MenuItem';
-import Menu from '@material-ui/core/Menu';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Logo from "../../assets/logo.png";
+import Menu from "@material-ui/core/Menu";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -35,25 +29,20 @@ export default function MenuAppBar() {
     setAuth(event.target.checked);
   };
 
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
   const handleClose = () => {
     setAnchorEl(null);
   };
 
   return (
     <div className={classes.root}>
-      {/* <FormGroup>
-        <FormControlLabel
-          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup> */}
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             {/* <MenuIcon /> */}
             <img src={Logo} height={75} width={75} />
           </IconButton>
@@ -62,33 +51,21 @@ export default function MenuAppBar() {
           </Typography>
           {auth && (
             <div>
-              {/* <IconButton
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton> */}
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorEl}
                 anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 open={open}
                 onClose={handleClose}
-              >
-                {/* <MenuItem onClick={handleClose}>Profile</MenuItem> */}
-                {/* <MenuItem onClick={handleClose}>My account</MenuItem> */}
-              </Menu>
+              ></Menu>
             </div>
           )}
         </Toolbar>
--- src/Components/Transactions/index.tsx
+++ src/Components/Transactions/index.tsx
@@ -1,7 +1,7 @@
-import React, { useState,useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "plaid-threads/Button";
 import Note from "plaid-threads/Note";
-import CircularProgress from '@material-ui/core/CircularProgress';
+import CircularProgress from "@material-ui/core/CircularProgress";
 import Table from "../Table";
 import Error from "../Error";
 import { DataItem, Categories, ErrorDataItem, Data } from "../../dataUtilities";
@@ -36,16 +36,20 @@ const Transactions = (props: Props) => {
     }
 
     // return console.log("checking data...",data);
-    
-    let result = props.transformData(data)
-    result.map((item:any,index:any)=>{
-      let balance:any
 
-      balance = '+ ' + (Math.ceil(item.amount.split(" ")[1].replace( ',', '')) - item.amount.split(" ")[1].replace( ',', '')).toFixed(2)
-      
-      result[index] = {...result[index],sats:balance} 
-      
-    })
+    let result = props.transformData(data);
+    result.map((item: any, index: any) => {
+      let balance: any;
+
+      balance =
+        "+ " +
+        (
+          Math.ceil(item.amount.split(" ")[1].replace(",", "")) -
+          item.amount.split(" ")[1].replace(",", "")
+        ).toFixed(2);
+
+      result[index] = { ...result[index], sats: balance };
+    });
     setTransformedData(result); // transform data into proper format for each individual product
     if (data.pdf != null) {
       setPdf(data.pdf);
@@ -54,57 +58,21 @@ const Transactions = (props: Props) => {
     setIsLoading(false);
   };
 
-  useEffect(()=>{
-    getData()
-  },[props])
+  useEffect(() => {
+    getData();
+  }, [props]);
 
   return (
     <>
-      {/* <div className={styles.endpointContainer}> */}
-        {/* <Note info className={styles.post}>
-          POST
-        </Note>
-        <div className={styles.endpointContents}>
-          <div className={styles.endpointHeader}>
-            {props.name != null && (
-              <span className={styles.endpointName}>{props.name}</span>
-            )}
-            <span className={styles.schema}>{props.schema}</span>
-          </div>
-          <div className={styles.endpointDescription}>{props.description}</div>
-        </div>
-        <div className={styles.buttonsContainer}>
-          <Button
-            small
-            centered
-            wide
-            secondary
-            className={styles.sendRequest}
-            onClick={getData}
-          >
-            {isLoading ? "Loading..." : `Send request`}
-          </Button>
-          {pdf != null && (
-            <Button
-              small
-              centered
-              wide
-              className={styles.pdf}
-              href={`data:application/pdf;base64,${pdf}`}
-              componentProps={{ download: "Asset Report.pdf" }}
-            >
-              Download PDF
-            </Button>
-          )}
-        </div>
-      </div> */}
       {showTable ? (
         <Table
           categories={props.categories}
           data={transformedData}
           isIdentity={props.endpoint === "identity"}
         />
-      ) : <CircularProgress disableShrink />}
+      ) : (
+        <CircularProgress disableShrink />
+      )}
       {/* {error != null && <Error error={error} />} */}
     </>
   );
